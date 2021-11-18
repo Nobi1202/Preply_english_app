2d07df6fed8a0ee633b62a2beb272e954f7e5a01
Nobi1202 <saobang001111@gmail.com>
2021-11-18 20:07:44 +0700

    format code

--- lib/components/content_model.dart
+++ lib/components/content_model.dart
@@ -3,23 +3,23 @@ class OnboardingContent {
   String title;
   String description;
 
-  OnboardingContent({required this.image, required this.title, required this.description});
+  OnboardingContent(
+      {required this.image, required this.title, required this.description});
 }
 
 List<OnboardingContent> contents = [
   OnboardingContent(
       title: 'Prepare to speak confidently',
       image: 'assets/images/pic1.PNG',
-      description: "Take personalized lessons with expert\n tutors and native speakers"
-  ),
+      description:
+          "Take personalized lessons with expert\n tutors and native speakers"),
   OnboardingContent(
       title: 'Learn with the best online tutors',
       image: 'assets/images/pic2.PNG',
-      description: "View tutor videos, profiles and reviews to\n choose the perfect tutor"
-  ),
+      description:
+          "View tutor videos, profiles and reviews to\n choose the perfect tutor"),
   OnboardingContent(
       title: 'Stay connected',
       image: 'assets/images/pic3.PNG',
-      description: "Message tutors and get notifications about\n your lessons"
-  ),
+      description: "Message tutors and get notifications about\n your lessons"),
 ];
